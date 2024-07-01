8fcc11e25c99368f0e41be8f45d3d9351f815737
GLorentzos <162353754+GLorentzos@users.noreply.github.com>
2024-07-01 14:58:33 +0300

    update3

--- js/server_players.js
+++ js/server_players.js
@@ -3,7 +3,7 @@ async function fetchPlayerCount() {
         const response = await fetch('https://servers-frontend.fivem.net/api/servers/single/4ryxoo');
         const data = await response.json();
         const playerCount = data.Data.players.length; 
-        document.getElementById('playerCount').innerText = `Players Online: ${playerCount}`;
+        document.getElementById('playerCount').innerText = `${playerCount}`;
     } catch (error) {
         console.error('Error fetching player data:', error);
         document.getElementById('playerCount').innerText = 'Could not fetch player data.';
