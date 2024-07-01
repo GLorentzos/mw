58c60e59a23ae8790d72a60311bd6151850dc0cb
GLorentzos <162353754+GLorentzos@users.noreply.github.com>
2024-07-01 15:05:31 +0300

    update5

--- js/server_players.js
+++ js/server_players.js
@@ -1,15 +1,18 @@
-async function fetchPlayerCount() {
+async function fetchPlayerData() {
     try {
         const response = await fetch('https://servers-frontend.fivem.net/api/servers/single/4ryxoo');
         const data = await response.json();
-        const playerCount = data.Data.players.length; 
-        document.getElementById('playerCount').innerText = `${playerCount}`;
+
+        const playerCount = data.Data.players.length;
+        const maxSlots = data.Data.sv_maxclients;
+
+        document.getElementById('playerCount').innerText = `${playerCount} / ${maxSlots}`;
     } catch (error) {
         console.error('Error fetching player data:', error);
         document.getElementById('playerCount').innerText = 'Could not fetch player data.';
     }
 }
 
-fetchPlayerCount();
+fetchPlayerData();
 
-setInterval(fetchPlayerCount, 30000);
+setInterval(fetchPlayerData, 30000);
